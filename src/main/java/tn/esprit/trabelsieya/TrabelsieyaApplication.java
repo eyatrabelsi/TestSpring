9b06d8411ac90eeb524880b1903c23da38d32c54
eyatrabelsi <eya.trabelsi@esprit.tn>
2022-11-23 09:31:54 +0100

    FirstCommit

---
+++ src/main/java/tn/esprit/trabelsieya/TrabelsieyaApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.trabelsieya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabelsieyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabelsieyaApplication.class, args);
+    }
+
+}
